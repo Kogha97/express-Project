b82e5309ba236510bb50b6ada0170853ea04ae2e
theMomo93 <mkaminska93@gmail.com>
2024-01-20 13:12:19 +0100

    first commit

---
+++ controllers/recipesController.js
@@ -0,0 +1,38 @@
+import writeData from "../utils/writeData.js";
+import readData from "../utils/readData.js";
+// LIST ALL
+export const listAll = (req, res) => {
+  console.log("recipes/all");
+
+  res.send("list all ok");
+};
+
+//LIST ONE
+export const listOne = (req, res) => {
+  console.log("recipes/one");
+  res.send("list one ok");
+};
+
+//ADD
+export const addRecipe = (req, res) => {
+  console.log("/add");
+//load Array
+  const arrayRecipe = readData();
+//store new info to array
+  arrayRecipe.push(req.body);
+//array to file
+  writeData(arrayRecipe);
+
+  res.send("add ok");
+};
+
+// DELETE
+export const deleteRecipe = (req, res) => {
+  console.log("/delete/one");
+  res.send("delete ok");
+};
+// EDIT
+export const editRecipe = (req, res) => {
+  console.log("/edit");
+  res.send("oedit k");
+};
---
+++ utils/writeData.js
@@ -0,0 +1,5 @@
+import * as fs from 'fs';
+
+export default function writeData(data) {
+
+fs.writeFileSync("data.json", JSON.stringify(data,null,2), "utf8")};
